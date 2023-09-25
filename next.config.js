712d8d85d400c8e293047f2c6fc884487863d44c
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2023-09-25 16:43:38 +0800

    fixed

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const nextConfig = {
     output: 'export',
     reactStrictMode: true,
     swcMinify: true,
-    images: {
-      unoptimized: true,
-    },
   };
   
   module.exports = nextConfig;
