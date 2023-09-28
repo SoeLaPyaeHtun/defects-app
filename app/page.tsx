063e26905ce9202b022adfd75a2432141fe64b1f
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2023-09-28 17:40:52 +0800

    m

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 'use client'
-import Head from 'next/head';
+
+
+
+
 import Camera from './components/Camera';
 
-
 export default function Home() {
-  
+
+
 
 
   return (
     <div className="container">
       <h1>Camera App</h1>
-    
-      <Camera/>
+
+      <Camera />
     </div>
   );
-  
+
 }
